28d277c850cb4cb530941bf2fae5814ebff6e9bd
AdamMatheus <adam.matheus.it@gmail.com>
2021-11-02 22:48:33 +0100

    ok

--- Color Flipper/hex.js
+++ Color Flipper/hex.js
@@ -1,8 +1,9 @@
 const hex=[0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
 
-//#f15025
+
 const btn=document.getElementById("btn");
 const color=document.querySelector(".color");
+
 
 btn.addEventListener("click",function(){
 let hexColor="#";
@@ -10,8 +11,11 @@ for (let i=0; i<6;i++){
     hexColor += hex[getRandomNumber()]
 }
 color.textContent=hexColor;
+
 document.body.style.backgroundColor=hexColor;
+document.body.main.button.style.color=hexColor;
 });
+
 
 function getRandomNumber(){
     return Math.floor(Math.random()*hex.length);
