e60380fec83f575f594028b7ddcbc49aef2bac4d
AdamMatheus <adam.matheus.it@gmail.com>
2021-11-27 22:56:00 +0100

    ok

---
+++ Review Carousel/app.js
@@ -0,0 +1,25 @@
+const reviews=[
+    {
+        id:1,
+        name:"John Mark",
+        job:"Driver",
+        img:URL("OIP1.jfif"),
+        text:Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil fugit fuga amet architecto consequatur officia sint exercitationem asperiores minus est.
+
+    },
+    {
+        id:2,
+        name:"Martin Dark",
+        job:"Developer",
+        img:URL("OIP2.jfif"),
+        text:Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil fugit fuga amet architecto consequatur officia sint exercitationem asperiores minus est.
+
+    },
+    {
+        id:3,
+        name:"John Mark",
+        job:"Manager",
+        img:URL("R.jfif"),
+        text:Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil fugit fuga amet architecto consequatur officia sint exercitationem asperiores minus est.
+    }
+];
